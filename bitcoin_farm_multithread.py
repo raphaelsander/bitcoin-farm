af83b5b0c9d91297883f089099233fb89abb023c
Raphael Sander <raphael.sander75@gmail.com>
2021-05-24 17:26:39 -0300

    Upgraded to Python3

--- bitcoin_farm_multithread.py
+++ bitcoin_farm_multithread.py
@@ -1,51 +1,73 @@
-#!/usr/bin/python2.7
-# -*- coding: utf-8 -*-
-
-## Dependencias
-# apt install python-bs4
+#!/usr/bin/python3
 
 import requests
-import random
 import urllib3
+import random
 from bs4 import BeautifulSoup
 from threading import Thread
+import json
+from time import ctime
 import os
 
-urllib3.disable_warnings()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 class Th(Thread):
     def __init__(self, num):
         Thread.__init__(self)
         self.num = num
+
     def run(self):
         while True:
             page = random.randint(0, 904625697166532776746648320380374280100293470930272690489102837043110636675)
             try:
-                req = requests.get("https://lbc.cryptoguru.org/dio/%s" %page, verify=False)
+                req = requests.get("https://lbc.cryptoguru.org/dio/%s" % page, verify=False)
                 if req.status_code == 200:
-                    print("Page: %s" %page)
+                    print("%s - Page: %s" % (ctime(), page))
                     content = req.content
                     soup = BeautifulSoup(content, 'html.parser')
                     all_span = soup.find_all(name='span')
-                    list = []
+                    list_addresses = []
                     for x in all_span:
-                        list.append(x.get_text().split())
-                    
+                        list_addresses.append(x.get_text().split())
+
+                    y = json.dumps(list_addresses)
+
+                    addresses = ""
                     for x in range(0, 256, 2):
-                        
-                        req2 = requests.get("https://blockchain.info/q/addressbalance/%s" %list[x][2])
+                        addresses += str(list_addresses[x][2])
 
-                        if req2.status_code == 200:
-                            content2 = req2.content
-                                
-                            if content2 != "0":
-                                output = ("PrivateKey: %s : PublicKey: %s" %(list[x][1], list[x][2]))
-                                print(output)
-                                os.system('echo %s >> keys.txt' %page)
-                                os.system('echo %s >> keys.txt' %output)
-            except:
-                print("Erro - Page: %s" %page)
+                        if x < 254:
+                            addresses += "|"
 
-for x in range(1, 20):
-	a = Th(x)
-	a.start()
+                    # print("%s - %s" % (ctime(), addresses))
+
+                    req2 = requests.get("https://blockchain.info/balance?active=%s" % addresses)
+                    content2 = req2.content
+
+                    # print("%s - %s" % (ctime(), content2))
+
+                    addresses2 = json.loads(content2.decode("utf-8"))
+                    for xy in addresses2:
+                        if addresses2['%s' % xy]['final_balance'] != 0:
+                            for xyz in range(0, 256, 2):
+                                if list_addresses[xyz][2] == xy:
+                                    output = ("PublicKey:%s Balance:%s PrivateKey:%s" % (
+                                        xy, addresses2['%s' % xy]['final_balance'], list_addresses[xyz][1]))
+                                    print(output)
+                                    os.system('echo %s >> /home/pi/keys.txt' % output)
+                        if addresses2['%s' % xy]['total_received'] != 0:
+                            for xyz in range(0, 256, 2):
+                                if list_addresses[xyz][2] == xy:
+                                    output = ("PublicKey:%s Received:%s PrivateKey:%s" % (
+                                        xy, addresses2['%s' % xy]['total_received'], list_addresses[xyz][1]))
+                                    print(output)
+                                    os.system('echo %s >> /home/pi/keys.txt' % output)
+
+            except TimeoutError:
+                print("%s - ERRO - Page: %s" % (ctime(), page))
+
+
+for x in range(0, 4):
+    a = Th(x)
+    a.start()
